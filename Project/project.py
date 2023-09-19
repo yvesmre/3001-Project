64117160f0062c70a18982dedbf21596ba4c4fb7
TechnoStasis <yvesmiguelreyes1@gmail.com>
2023-09-19 11:30:11 +0800

    OpenCV example code

--- Project/project.py
+++ Project/project.py
@@ -2,18 +2,27 @@ from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 import gym
+import time
 
 
-env = gym.make('SuperMarioBros-v3', apply_api_compatibility=True, render_mode="human")
+env = gym.make('SuperMarioBros-v0', apply_api_compatibility=True, render_mode="human")
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
 done = True
 env.reset()
 
-
-for step in range(5000):
+print (env.buttons)
+while True:
+        action_space = env.action_space
+        observation_space = env.observation_space
+    
+        
         action = env.action_space.sample()
-        obs, reward, terminated, truncated, info = env.step(action)
+
+        obs, reward, terminated, truncated, info = env.step(2)
         done = terminated or truncated
+
+        
+        time.sleep(.0166666667)
         if done:
             state = env.reset()
 env.close()
